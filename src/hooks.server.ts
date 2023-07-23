860b8b16d976f3018577da643359d58674676683
Foxtrot47 <jjneutron@outlook.com>
2023-07-23 17:35:14 +0530

    Setup Auth.js

---
+++ src/hooks.server.ts
@@ -0,0 +1,65 @@
+import { SvelteKitAuth } from '@auth/sveltekit';
+import GitHub from '@auth/core/providers/github';
+import Google from '@auth/core/providers/google';
+import Credentials from '@auth/core/providers/credentials';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import prisma from '$lib/prisma';
+import {
+	GITHUB_ID,
+	GITHUB_SECRET,
+	GOOGLE_CLIENT_ID,
+	GOOGLE_CLIENT_SECRET
+} from '$env/static/private';
+import bcrypt from 'bcrypt';
+
+export const handle = SvelteKitAuth({
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		GitHub({
+			clientId: GITHUB_ID,
+			clientSecret: GITHUB_SECRET
+		}),
+		Google({
+			clientId: GOOGLE_CLIENT_ID,
+			clientSecret: GOOGLE_CLIENT_SECRET
+		}),
+		Credentials({
+			name: 'credentials',
+			credentials: {
+				email: { label: 'email', type: 'email' },
+				password: { label: 'password', type: 'password' }
+			},
+			async authorize(credentials) {
+				if (!credentials?.email || !credentials?.password) {
+					return null;
+				}
+
+				const user = await prisma.user.findUnique({
+					where: { email: credentials.email.toString() }
+				});
+				if (!user || !user?.hashedPassword) {
+					return null;
+				}
+
+				const isCorrectPassword = await bcrypt.compare(
+					credentials.password.toString(),
+					user.hashedPassword
+				);
+
+				if (!isCorrectPassword) {
+					return null;
+				}
+
+				return user;
+			}
+		})
+	],
+	pages: {
+		signIn: '/'
+	},
+	debug: process.env.NODE_ENV === 'development',
+	session: {
+		strategy: 'jwt'
+	},
+	secret: process.env.NEXTAUTH_SECRET
+});
