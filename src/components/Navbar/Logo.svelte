62002731b1c26ee08f02d254aa33e96ba2f20414
Foxtrot47 <jjneutron@outlook.com>
2023-07-25 22:28:10 +0530

    Link logo to home page

--- src/components/Navbar/Logo.svelte
+++ src/components/Navbar/Logo.svelte
@@ -1,3 +1,11 @@
 <svelte>
-	<img alt="logo" class="hidden cursor-pointer md:block" height="100" width="100" src="/logo.png" />
+	<a href="/">
+		<img
+			alt="logo"
+			class="hidden cursor-pointer md:block"
+			height="100"
+			width="100"
+			src="/logo.png"
+		/>
+	</a>
 </svelte>
