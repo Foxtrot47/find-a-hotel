c68b3f3349cf70a63f7bf9f588e8b0965a1fc874
Foxtrot47 <jjneutron@outlook.com>
2023-07-28 00:24:07 +0530

    Make Modal Buttons appear in a row

--- src/components/Modals/Modal.svelte
+++ src/components/Modals/Modal.svelte
@@ -68,7 +68,7 @@
 					</div>
 					<!-- Footer -->
 					<div class="flex flex-col gap-2 p-6">
-						<div class="w-full flex-row items-center gap-4">
+						<div class="w-full flex flex-row items-center gap-4">
 							{#if secondaryActionLabel}
 								<Button
 									{disabled}
